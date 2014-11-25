968ae8f8c266bd77cc4f3f55efe8cd72157c4fb8
grantbot <grantwu2@gmail.com>
2014-11-24 17:23:28 -0800

    added html and get logic for loading \links

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -2,4 +2,13 @@ angular.module('shortly.links', [])
 
 .controller('LinksController', function ($scope, Links) {
   // Your code here
+  angular.extend($scope,Links);
+  $scope.data.push('empty1')
+  $scope.data.push('empty2');
+  $scope.getLinks()
+  .then(function (data){
+    $scope.data.concat(data);
+  })
+  .catch(function(er) {console.error(er);});
+
 });
--- client/app/services/services.js
+++ client/app/services/services.js
@@ -2,6 +2,21 @@ angular.module('shortly.services', [])
 
 .factory('Links', function ($http) {
   // Your code here
+  var data = []
+  var getLinks = function () {
+    //ajax request here
+    return $http({
+      method: 'GET',
+      url: '/api/links'
+    })
+    .then(function(res){
+      return res.data
+    })
+  };
+  return {
+    data: data,
+    getLinks : getLinks
+  };
 })
 .factory('Auth', function ($http, $location, $window) {
   // Don't touch this Auth service!!!
